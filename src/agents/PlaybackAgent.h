e75c8731ea115c5141a8e73e3e57071f66b389c4
hkweijieqwewqewqe <107761765+hkweijieqwewqewqe@users.noreply.github.com>
2025-06-11 02:37:52 +0800

    Add C++ agent skeleton

---
+++ src/agents/PlaybackAgent.h
@@ -0,0 +1,12 @@
+#ifndef PLAYBACK_AGENT_H
+#define PLAYBACK_AGENT_H
+
+class PlaybackAgent {
+public:
+    PlaybackAgent() = default;
+    // Control playback of audio
+    void play();
+    void pause();
+};
+
+#endif // PLAYBACK_AGENT_H
